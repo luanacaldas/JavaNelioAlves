06394cac2efd461ef9e9a3b657da72e626c17f26
luanacaldas <102559861+luanacaldas@users.noreply.github.com>
2023-08-17 21:25:26 -0300

    Update uri1008.java

--- src/uri1008.java
+++ src/uri1008.java
@@ -1,9 +1,14 @@
-import java.util.Locale;
+/*
+   04) Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+decimais.
+*/
+
 import java.util.Scanner;
+import java.util.Locale;
 
-public class uri1008 {
-
-	public static void main(String[] args) {
+public class Ex01JavaNelio {   
+public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
@@ -11,8 +16,11 @@ public class uri1008 {
 		int numero, horas;
 	    double valorHora, salario;
 
+       System.out.println("Digite o número do funcionário:");
 	    numero = sc.nextInt();
+       System.out.println("Digite as horas trabalhadas do funcionário:");
 	    horas = sc.nextInt();
+       System.out.println("Digite o valor que recebe por hora:");
 	    valorHora = sc.nextDouble();
 
 	    salario = valorHora * horas;
