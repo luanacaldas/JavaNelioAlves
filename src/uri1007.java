d79000a1a57959ba1cc158a152e46fffb52120e4
luanacaldas <102559861+luanacaldas@users.noreply.github.com>
2023-08-17 21:17:35 -0300

    Update uri1007.java

--- src/uri1007.java
+++ src/uri1007.java
@@ -1,22 +1,32 @@
+/*
+   03) Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+Ex - Seção 4 - Estrutura sequencial
+*/
+
 import java.util.Scanner;
 
-public class uri1007 {
-
-	public static void main(String[] args) {
-
+public class Ex01JavaNelio {   
+public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		
-		int A, B, C, D, dif;
 
-	    A = sc.nextInt();
-	    B = sc.nextInt();
-	    C = sc.nextInt();
-	    D = sc.nextInt();
+		int A, B, C, D, diferença;
 
-	    dif = A * B - C * D;
+      System.out.print("Digite o primeiro valor: ");
+		A = sc.nextInt();
+      System.out.print("Digite o segundo valor: ");
+		B = sc.nextInt();
+      System.out.print("Digite o terceiro valor: ");
+		C = sc.nextInt();
+      System.out.print("Digite o quarto valor: ");
+		D = sc.nextInt();
 
-	    System.out.println("DIFERENCA = " + dif);	
-		
-		sc.close();
+		diferença = A * B - C * D;
+
+		System.out.println("DIFERENCA = " + diferença);
+
+		sc.close ();
 	}
 }
+
