aade4f458b55f28faaeccab48d615f3fb5e7b0c8
luanacaldas <102559861+luanacaldas@users.noreply.github.com>
2023-08-17 21:16:07 -0300

    Update uri1003.java

--- src/uri1003.java
+++ src/uri1003.java
@@ -1,20 +1,27 @@
+/*
+   01) Programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa.
+Ex - Seção 4 - Estrutura sequencial
+*/
+
 import java.util.Scanner;
 
-public class uri1003 {
-
-	public static void main(String[] args) {
+public class Ex01JavaNelio {   
+public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 
 		int A, B, soma;
 
+      System.out.print("Digite o primeiro valor inteiro: ");
 		A = sc.nextInt();
+      System.out.print("Digite o segundo valor inteiro: ");
 		B = sc.nextInt();
 
 		soma = A + B;
 
 		System.out.println("SOMA = " + soma);
 
-		sc.close();
+		sc.close ();
 	}
 }
