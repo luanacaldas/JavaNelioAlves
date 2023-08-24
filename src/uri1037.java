3cbcbe95c05f88dc63828ad42892086df8aea648
luanacaldas <102559861+luanacaldas@users.noreply.github.com>
2023-08-24 17:51:57 -0300

    Update uri1037.java

--- src/uri1037.java
+++ src/uri1037.java
@@ -1,31 +1,29 @@
-import java.util.Locale;
 import java.util.Scanner;
+import java.util.Locale;
+public class Ex01JavaNelio {
+public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
 
-public class uri1037 {
-
-	public static void main(String[] args) {
-
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-
-		double numero = sc.nextDouble();
-		
-		if (numero < 0.0 || numero > 100.0) {
-			System.out.println("Fora de intervalo");
-		}
-		else if (numero <= 25.0) {
-			System.out.println("Intervalo [0,25]");
-		}
-		else if (numero <= 50.0) {
-			System.out.println("Intervalo (25,50]");
-		}
-		else if (numero <= 75.0) {
-			System.out.println("Intervalo (50,75]");
-		}		
-		else {
-			System.out.println("Intervalo (75,100]");
-		}
-		
-		sc.close();
-	}
+        System.out.print("Digite um número: ");
+        double numero = scanner.nextDouble();
+        
+        if (numero < 0.0 || numero > 100.0) {
+            System.out.println("Fora de intervalo");
+        }
+        else if (numero <= 25.0) {
+            System.out.println("Intervalo [0, 25]");
+        }
+        else if (numero <= 50.0) {
+            System.out.println("Intervalo (25, 50]");
+        }
+        else if (numero <= 75.0) {
+            System.out.println("Intervalo (50, 75]");
+        }        
+        else {
+            System.out.println("Intervalo (75, 100]");
+        }
+        
+        scanner.close();
+    }
 }
